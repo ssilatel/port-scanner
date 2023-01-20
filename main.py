1832872b2fb99ae416c1c9473037ba990374cd0e
ssilatel <setaxe25@gmail.com>
2023-01-20 13:37:58 +0100

    Implemented outputting results to file

--- main.py
+++ main.py
@@ -10,11 +10,19 @@ class App:
                     ports=self.cli_args.ports,
                     timeout=self.cli_args.timeout
                 )
+        self.output_file = self.cli_args.output if self.cli_args.output else None
 
     def scan(self):
         print(f"Starting scan on {self.port_scanner.target}\n")
-        for port in self.port_scanner.scan_ports():
-            print(port)
+        if self.output_file:
+            with open(self.output_file, mode="w", encoding="utf-8") as f:
+                for port in self.port_scanner.scan_ports():
+                    f.write(f"{str(port)}\n")
+                    print(port)
+        else:
+            for port in self.port_scanner.scan_ports():
+                print(port)
+            
 
 
 if __name__ == "__main__":
