f7b677b9e084b42ecd88057ea22b3d1be1c4da1f
ssilatel <setaxe25@gmail.com>
2023-01-22 11:38:10 +0100

    Implementing multithreading

--- src/tcp_scanner.py
+++ src/tcp_scanner.py
@@ -1,5 +1,7 @@
 from collections.abc import Collection
+from concurrent.futures import ThreadPoolExecutor
 from src.types import PortStatus, Port, ScanResults
+from threading import Lock
 import socket
 
 
@@ -17,6 +19,7 @@ class TCPScanner:
         self.output_file = output_file
         self.scan_results = ScanResults([])
         self.observers = []
+        self.lock = Lock()
 
     def attach(self, observer):
         self.observers.append(observer)
@@ -25,23 +28,27 @@ class TCPScanner:
         for observer in self.observers:
             observer.update()
     
-    def scan_ports(self):
-        for p in self.ports:
+    def scan_single_port(self, port):
+        with self.lock:
             with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
                 try:
                     sock.settimeout(self.timeout)
-                    sock.connect((self.target, p))
+                    sock.connect((self.target, port))
                 except socket.gaierror:
                     raise SystemExit(
                         f"Failed to connect or resolve hostname to target "
                         f"address {self.target}"
                     )
                 except socket.timeout:
-                    self.scan_results.ports.append(Port(p, PortStatus.TIMEOUT))
+                    self.scan_results.ports.append(Port(port, PortStatus.TIMEOUT))
                     self.notify()
                 except ConnectionRefusedError:
-                    self.scan_results.ports.append(Port(p, PortStatus.CONN_REFUSED))
+                    self.scan_results.ports.append(Port(port, PortStatus.CONN_REFUSED))
                     self.notify()
                 else:
-                    self.scan_results.ports.append(Port(p, PortStatus.OPEN))
+                    self.scan_results.ports.append(Port(port, PortStatus.OPEN))
                     self.notify()
+
+    def scan_ports(self):
+        with ThreadPoolExecutor(max_workers=10) as executor:
+            executor.map(self.scan_single_port, self.ports)
